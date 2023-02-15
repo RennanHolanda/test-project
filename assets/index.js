d9d4f15c34e9ae7319f821a3168fcfe8da69ae15
Bruno Rennan <bruno_rennan@hotmail.com>
2023-02-15 14:57:59 -0300

    refatoração

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,10 @@
 
-let data = new Date()
 
 let novoElemento = document.getElementById('texto')
+
+let novo = document.createTextNode(new Date())
+
+novoElemento.appendChild(novo)
 
 console.log(data)
 console.log(novoElemento)
